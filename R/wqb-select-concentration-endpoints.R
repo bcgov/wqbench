f19ee70d71f964fd7d9f2d5e6f6b3fb6983068c4
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-08 12:08:28 -0600

    adding start of conc selection file

---
+++ R/wqb-select-concentration-endpoints.R
@@ -0,0 +1,15 @@
+#' Select only Concentration Related Results 
+#' 
+#' Determine the result ID's from concentration based endpoint tests. 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+wqb_select_concentration_endpoints <- function(file_path, database) {
+  
+}
+### I think we should write the selected endpoints into a lookup table so it
+### easy to update and transparent as to which ones are actually being used.
+
+
