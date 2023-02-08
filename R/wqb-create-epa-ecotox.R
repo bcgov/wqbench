7d9789eafa067fd6c391906e37164e8faac6b367
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-08 17:29:15 -0600

    start framing out how to compile and build the database

--- R/wqb-create-epa-ecotox.R
+++ R/wqb-create-epa-ecotox.R
@@ -1,11 +1,42 @@
-#' Create a SQLite/PostgreSQL Database from the US EPA ECOTOX data
+#' Create a SQLite Database from the US EPA ECOTOX data
 #'
-#' @return
+#'  Use this function to create a local SQLite database of the ECOTOX data.
+#'
+#' @param file_path A string of the file path location to save the SQLite 
+#'  database
+#' @param data_path A string of the location the downloaded ECOTOX data folder
+#'
+#' @return Invisible string of the file path of the database.
 #' @export
 #'
 #' @examples
-wqb_create_epa_ecotox <- function(file_path = ".") {
+#' \dontrun{
+#' wqb_create_epa_ecotox(data_path = "ecotox_ascii_12_15_2022")
+#' }
+wqb_create_epa_ecotox <- function(file_path = ".", data_path) {
+  chk::chk_string(file_path)
+  chk::chk_string(data_path)
   
+  dbname <- paste0(basename(source), ".sqlite")
+  dbfile <- file_path(file_path, dbname)
+  con  <- DBI::dbConnect(
+    RSQLite::SQLite(), 
+    dbfile
+  )
+  
+  
+  files <- list.files(
+    data_path,
+    pattern = "*.txt", 
+    full.names = TRUE
+  )
+  
+  files_data <- files[!grepl('release', files)]
+  name_data <- gsub(".txt", "", basename(files_data))
+  
+  DBI::dbDisconnect(conn)
+  
+  invisible(dbfile)
 }
 
 ### Inputs
