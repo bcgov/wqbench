66f98ea37d1eb036c7de208805880a76a61333b5
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-11 11:58:56 -0600

    adding simple edge case tests

--- tests/testthat/test-wqb-download-epa-ecotox.R
+++ tests/testthat/test-wqb-download-epa-ecotox.R
@@ -1,6 +1,17 @@
-test_that("inputs don't error", {
-  ### very basic testing for now, until function built
-  #expect_type(wqb_download_epa_ecotox(), "character")
+test_that("error when version beyond 4 passed", {
+  expect_error(
+    wqb_download_epa_ecotox(version = 5),
+    regexp = "`version` must be between 1 and 4, not 5\\."
+  )
+})
+
+test_that("error when version lower then 1 is passed", {
+  expect_error(
+    wqb_download_epa_ecotox(version = 0),
+    regexp = "`version` must be between 1 and 4, not 0\\."
+  )
 })
 
 ### may need to use withr to creates temp directory to test this step
+### those tests will be slow as the db is large to download
+### it could fail pending connection and other issues
