3f492069c06946f788b1c65793d36ae6e8e4d2f0
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-18 14:47:38 -0600

    docs

--- R/wqb-add-bc-species.R
+++ R/wqb-add-bc-species.R
@@ -1,26 +1,25 @@
 #' Add BC Species to Database
-#' 
-#' Read in the British Columbia species values and add them to the SQLite 
-#' database. 
+#'
+#' Read in the British Columbia species values and add them to the SQLite
+#' database.
 #'
 #' @param database A string to the location of the database.
 #' @return Invisible data frame
 #' @export
-#' @details The BC species data is contained in a csv file in the extdata 
-#' folder of the package.
-#' This file can be edited by adding new species or removing species. Do not
-#' add new columns, rename columns or rename the file. The file must only 
-#' contain a single column named `latin_name`.
-#' 
-#' The `latin_name` column must consist of the genus and species separated by a 
-#' space. The `latin_name` column in the bc-species file are matched to the
-#' `latin_name` column in the species table of the ECOTOX downloaded data.
-#' 
-#' The output table that is written to the database contains two columns: 
-#' species_number and bc_species 
-#' 
-#' The output table is added to the database with the name 
-#' `species_british_columbia`.
+#' @details The BC species data is contained in a csv file in the extdata folder
+#'   of the package. This file can be edited by adding new species or removing
+#'   species. Do not add new columns, rename columns or rename the file. The
+#'   file must only contain a single column named `latin_name`.
+#'
+#'   The `latin_name` column must consist of the genus and species separated by
+#'   a space. The `latin_name` column in the bc-species file are matched to the
+#'   `latin_name` column in the species table of the ECOTOX downloaded data.
+#'
+#'   The output table that is written to the database contains two columns:
+#'   species_number and bc_species
+#'
+#'   The output table is added to the database with the name
+#'   `species_british_columbia`.
 #' @examples
 #' \dontrun{
 #' bc_species <- wqb_add_bc_species(
--- R/wqb-add-habitat.R
+++ R/wqb-add-habitat.R
@@ -1,21 +1,16 @@
-#' Add Habitat Information for each Species to Database
-#'
-#' Aggregate the sub habitat info from the tests table for each species.
-#' Habitats are classified into three main groups: freshwater, brackish and
-#' marine. Adds the resulting table into the database. 
+#' Code Media Type to Salt or Fresh Water by Species and Test
+#' 
+#' The media type code for each species and test has been coded to either salt water, 
+#' fresh water or not reported. 
 #'
 #' @param database A string to the location of the database.
 #' @return Invisible data frame
 #' @export
-#' @details This function reads in the test table and aggregates the sub 
-#'  habitat column data for each aquatic species. 
-#'  
-#'  The output table contains four columns: species number, freshwater, brackish 
-#'  and marine. If the species was reported to be from a certain habitat the 
-#'  value has been coded as TRUE. Species can be reported to be from more then 
-#'  one habitat type. 
+#' @details This function reads in the test table and codes each value to one 
+#'   of three options: salt water, fresh water or not reported.
 #' 
-#'  The output table is added to the database with the name `species_habitat`.
+#'  The output table is added to the database with the name 
+#'  `species_tests_media`.
 #' @examples
 #' \dontrun{
 #' habitat_info <- wqb_add_habitat(
@@ -27,5 +22,57 @@
 #' ) 
 #' }
 wqb_add_habitat <- function(database) {
-  chk::chk_string(database)
+  chk::chk_file(database)
+  chk::chk_ext(database, "sqlite")
+  
+  # read in tables from db
+  on.exit(DBI::dbDisconnect(con))
+  con  <- DBI::dbConnect(
+    RSQLite::SQLite(), 
+    database
+  )
+  db_tests <- DBI::dbReadTable(con, "tests")
+  db_media_type <- DBI::dbReadTable(con, "media_type_codes")
+  
+  chk::check_data(
+    db_tests, 
+    list(
+      species_number = 1L,
+      media_type = ""
+    )
+  )
+  
+  species_tests_media <- db_tests |>
+    dplyr::left_join(db_media_type, by = c("media_type" = "code")) |>
+    dplyr::mutate(
+      media_type_code = dplyr::case_when(
+        .data[["description"]] == "Fresh water" ~ "fresh water",
+        .data[["description"]] == "Salt water" ~ "salt water",
+        TRUE ~ "not reported"
+      )
+    ) |>
+    dplyr::select(
+      "species_number", "test_id", "media_type_code"
+    ) |>
+    dplyr::arrange(.data[["species_number"]], .data[["test_id"]]) |>
+    tibble::tibble()
+  
+  DBI::dbExecute(
+    con,
+    paste0(
+      "CREATE TABLE species_tests_media ",
+      "(species_number, test_id, media_type_code, ",
+      "PRIMARY KEY (species_number, test_id))"
+    )
+  )
+  
+  DBI::dbWriteTable(
+    con, 
+    "species_tests_media", 
+    value = species_tests_media, 
+    append = TRUE, 
+    row.names = FALSE
+  )
+  
+  invisible(species_tests_media)
 }
