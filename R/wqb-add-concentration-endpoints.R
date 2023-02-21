f7fe60cfea37f4ef525e542704e2102d1769f8df
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-21 13:34:35 -0800

    updating concentration endpoint function to add the concentration flag to the endpoint table instead of creating its own table

--- R/wqb-add-concentration-endpoints.R
+++ R/wqb-add-concentration-endpoints.R
@@ -1,25 +1,25 @@
-#' Add the Concentration Endpoint List to Database
-#' 
-#' Add the list of selected concentration endpoints into the Database. 
+#' Add the Concentration Endpoints to Database
+#'
+#' Read in the selected concentration endpoints and mark those values in the
+#' endpoint_code table in the database by adding a `concentration_flag` column
+#' to the table.
 #'
 #' @param database A string to the location of the database.
 #' @return Invisible data frame
 #' @export
-#' @details The list of concentration endpoints to be used is contained in a csv 
-#'   file in the extdata folder of the package. The csv file can be edited by 
-#'   adding or removing rows.  To add new
-#'   rows get the `code` and `description` values from the `endpoint_code`
-#'   table in the ECOTOX data and paste them into the csv file.
-#'   
+#' @details The list of concentration endpoints to be used is contained in a csv
+#'   file in the extdata folder of the package. The csv file can be edited by
+#'   adding or removing rows.  To add new rows get the `code` and `description`
+#'   values from the `endpoint_code` table in the ECOTOX data and paste them
+#'   into the csv file.
+#'
 #'   Do not add new columns, rename columns or rename the file. The file must
-#'   only contain the `code` and `description` column. 
+#'   only contain the `code` and `description` column.
 #'
-#'   The output table that is written to the database contains one columns:
-#'   `code`.
-#'
-#'   The output table is added to the database with the name 
-#'   `endpoint_concentration`.
-#'   
+#'   The `code` values in the endpoint-concentration file are matched to the
+#'   `code` values in the endpoint_code table in the ECOTOX downloaded data. Any
+#'   codes that are listed in the file are then coded as TRUE under a new column
+#'   called `concentration_flag` in the endpoint_table.
 #' @examples
 #' \dontrun{
 #' wqb_add_concentration_endpoints(
@@ -34,23 +34,6 @@ wqb_add_concentration_endpoints <- function(database) {
   chk::chk_file(database)
   chk::chk_ext(database, "sqlite")
   
-  # read in bc species 
-  conc_endpoints_file_path <- system.file(
-    "extdata/concentration-endpoints.csv",
-    package = "wqbench"
-  )
-  endpoint_concentration <- readr::read_csv(
-    conc_endpoints_file_path, 
-    show_col_types = FALSE
-  ) 
-  chk::check_data(endpoint_concentration, list(code = ""))
-  
-  endpoint_concentration <- endpoint_concentration |>
-    dplyr::mutate(
-      code = stringr::str_squish(.data$code)
-    ) |>
-    dplyr::select("code")
-  
   # read in data from db
   on.exit(DBI::dbDisconnect(con))
   con  <- DBI::dbConnect(
@@ -62,18 +45,52 @@ wqb_add_concentration_endpoints <- function(database) {
       code = stringr::str_squish(.data$code)
     )
   
-  # print out name of any codes that don't match the db ones
-  matches <- !(endpoint_concentration$code %in% db_endpoint_code$code)
-  if (any((matches))) {
-    print("Value(s) do not match code(s) in `endpoint_code` table in ECOTOX database:")
-    print(endpoint_concentration$code[matches])
+  if ("concentration_flag" %in% colnames(db_endpoint_code)) {
+    stop(
+      "Concentration endpoint flag has already been added to the database"
+    )
   }
+  
+  # read in bc species 
+  conc_endpoints_file_path <- system.file(
+    "extdata/concentration-endpoints.csv",
+    package = "wqbench"
+  )
+  endpoint_concentration_pick <- readr::read_csv(
+    conc_endpoints_file_path, 
+    show_col_types = FALSE
+  ) 
+  chk::check_data(endpoint_concentration_pick, list(code = ""))
+  
+  # add concentration endpoints to endpoints table
+  endpoint_concentration_pick <- endpoint_concentration_pick |>
+    dplyr::mutate(
+      code = stringr::str_squish(.data$code),
+      concentration_flag = TRUE
+    ) |>
+    dplyr::select("code", "concentration_flag")
+  
+  # print out name of any codes that don't match the db ones
+  dont_match <- !(endpoint_concentration_pick$code %in% db_endpoint_code$code)
+  if (any((dont_match))) {
+    print("Value(s) do not match code(s) in `endpoint_code` table in ECOTOX database:")
+    print(endpoint_concentration_pick$code[dont_match])
+  }
+  
+  endpoint_concentration <- db_endpoint_code |> 
+    dplyr::left_join(endpoint_concentration_pick, by = "code") |>
+    dplyr::mutate(
+      concentration_flag =  tidyr::replace_na(.data$concentration_flag, FALSE)
+    ) |>
+    tibble::tibble()
   
   # write new table to database
   DBI::dbExecute(
     con,
-    paste0("CREATE TABLE endpoint_concentration ",
-           "(code TEXT, PRIMARY KEY (code))"
+    paste0(
+      "CREATE TABLE endpoint_concentration (",
+      "code TEXT, description TEXT, concentration_flag TEXT, ",
+      "PRIMARY KEY (code))"
     )
   )
   DBI::dbWriteTable(
@@ -82,6 +99,13 @@ wqb_add_concentration_endpoints <- function(database) {
     value = endpoint_concentration, 
     append = TRUE, 
     row.names = FALSE
+  )
+  
+  DBI::dbExecute(con, "DROP TABLE endpoint_codes;")
+  DBI::dbExecute(
+    con,
+    "ALTER TABLE endpoint_concentration
+  RENAME TO endpoint_codes;"
   )
   
   invisible(endpoint_concentration)
