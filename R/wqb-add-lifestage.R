6b7d03ccdc5d77d97cf265d118492bb26d923527
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-21 16:06:48 -0800

    updating add lifestage function to append the info on the lifestage_code table instead of creating its own function

--- R/wqb-add-lifestage.R
+++ R/wqb-add-lifestage.R
@@ -1,14 +1,16 @@
-#' Add corrected Life stage codes to Database
+#' Add Life Stage groups to Database
 #'
-#' Code the ECOTOX life stage code values into three simple life stages: ELS
-#' (early life stage), juveniles and adults.
+#' Read in the life stage simple groups and mark those values in the
+#' lifestage_code table in the database by adding a `simple_lifestage` column to
+#' the table.
 #'
 #' @param database A string to the location of the database.
 #' @return Invisible data frame
 #' @export
 #' @details  Only life stages related to fish or amphibians have been coded. The
 #'   purpose of the coding is to be able to simplify the many life stages into
-#'   three main categories.
+#'   three categories: els (early life stage), juveniles and adults. Not all
+#'   life stages have been coded into these three groups.
 #'
 #'   The life stage data is contained in a csv file in the extdata folder of the
 #'   package. The csv file can be edited by adding or removing rows.  To add new
@@ -20,10 +22,9 @@
 #'   only contain the `code`, `description_lifestage` and `simple_lifestage`
 #'   column.
 #'
-#'   The output table that is written to the database contains two columns:
-#'   `code` and `simple_lifestage`
-#'
-#'   The output table is added to the database with the name `lifestage_groups`.
+#'   The `code` values in the lifestage-codes.csv file are matched to the `code`
+#'   values in the lifestage_code table in the ECOTOX downloaded data. Any codes
+#'   that match are coded in a new column called `simple_lifestage`.
 #' @examples
 #' \dontrun{
 #' lifestage_codes <- wqb_add_lifestage(
@@ -38,7 +39,22 @@ wqb_add_lifestage <- function(database) {
   chk::chk_file(database)
   chk::chk_ext(database, "sqlite")
   
-  # read in trophic groups 
+  # read in table from db
+  on.exit(DBI::dbDisconnect(con))
+  con  <- DBI::dbConnect(
+    RSQLite::SQLite(), 
+    database
+  )
+  db_lifestage_codes <- DBI::dbReadTable(con, "lifestage_codes") |>
+    dplyr::mutate(
+      code = stringr::str_squish(.data$code)
+    )
+  if ("simple_lifestage" %in% colnames(db_lifestage_codes)) {
+    stop(
+      "Simple lifestage has already been added to the database"
+    )
+  }
+  # read in life stage groups 
   lifestage_file_path <- system.file(
     "extdata/lifestage-codes.csv",
     package = "wqbench"
@@ -55,44 +71,48 @@ wqb_add_lifestage <- function(database) {
       simple_lifestage = ""
     )
   )
-  
   lifestage_codes <- lifestage_codes |>
     dplyr::mutate(
       code = stringr::str_squish(.data$code),
       simple_lifestage = stringr::str_squish(.data$simple_lifestage),
       simple_lifestage = stringr::str_to_lower(.data$simple_lifestage)
-    )
-  
-  # read in tables from db
-  on.exit(DBI::dbDisconnect(con))
-  con  <- DBI::dbConnect(
-    RSQLite::SQLite(), 
-    database
-  )
-  db_lifestage_codes <- DBI::dbReadTable(con, "lifestage_codes")
+    ) |>
+    dplyr::select("code", "simple_lifestage")
+  # print out name of any codes that don't match the db ones
+  dont_match <- !(lifestage_codes$code %in% db_lifestage_codes$code)
+  if (any((dont_match))) {
+    print("Value(s) do not match code(s) in `endpoint_code` table in ECOTOX database:")
+    print(lifestage_codes$code[dont_match])
+  }
   
   lifestage_groups <- db_lifestage_codes |>
     dplyr::mutate(
       code = stringr::str_squish(.data$code)
     ) |>
     dplyr:: left_join(lifestage_codes, by = "code") |>
-    dplyr::select("code", "simple_lifestage") |>
-    tibble::tibble() |>
-    tidyr::drop_na("simple_lifestage") 
+    tibble::tibble() 
   
+  # write new table to database
   DBI::dbExecute(
     con,
-    paste0("CREATE TABLE lifestage_groups ",
-           "(code TEXT, simple_lifestage TEXT, PRIMARY KEY (code))"
+    paste0(
+      "CREATE TABLE lifestage_groups ",
+      "(code TEXT, description TEXT, simple_lifestage TEXT, ",
+      "PRIMARY KEY (code))"
     )
   )
-  
   DBI::dbWriteTable(
     con, 
     "lifestage_groups", 
     value = lifestage_groups, 
     append = TRUE, 
     row.names = FALSE
+  )
+  DBI::dbExecute(con, "DROP TABLE lifestage_codes;")
+  DBI::dbExecute(
+    con,
+    "ALTER TABLE lifestage_groups
+  RENAME TO lifestage_codes;"
   )
   
   invisible(lifestage_groups)
