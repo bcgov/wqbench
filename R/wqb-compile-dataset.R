616f27bc10c5e789ac0502b8b6a819e132339056
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-24 15:53:00 -0800

    dealing with r cmd notes for tidy select verbs

--- R/wqb-compile-dataset.R
+++ R/wqb-compile-dataset.R
@@ -43,46 +43,41 @@ wqb_compile_dataset <- function(database) {
     ) |>
     tibble::tibble()
   db_species <- DBI::dbReadTable(con, "species") |>
-    dplyr::mutate(present_in_bc = as.logical(as.numeric(present_in_bc))) |>
+    dplyr::mutate(
+      present_in_bc = as.logical(as.numeric(.data$present_in_bc))
+    ) |>
     tibble::tibble()
   db_lifestage_codes <- DBI::dbReadTable(con, "lifestage_codes") |>
-    dplyr::rename(lifestage_description = description) |>
+    dplyr::rename("lifestage_description" = "description") |>
     tibble::tibble()
   db_chemicals <- DBI::dbReadTable(con, "chemicals") |>
     dplyr::mutate(
-      present_in_bc_wqg = as.logical(as.numeric(present_in_bc_wqg))
+      present_in_bc_wqg = as.logical(as.numeric(.data$present_in_bc_wqg))
     ) |>
     tibble::tibble()
   
   db_references <- DBI::dbReadTable(con, "references")
   db_effect_codes <- DBI::dbReadTable(con, "effect_codes") |>
-    dplyr::rename(effect_description = description) |>
+    dplyr::rename("effect_description" = "description") |>
     tibble::tibble()
   db_media_type_codes <- DBI::dbReadTable(con, "media_type_codes") |>
-    dplyr::rename(media_description = description) |>
+    dplyr::rename("media_description" = "description") |>
     tibble::tibble()
   
   combined_data <- db_results |>
     # filter to only water  (aquatic) tests
     dplyr::left_join(db_tests, by = "test_id") |>
-    dplyr::filter(organism_habitat == "Water") |>
+    dplyr::filter(.data$organism_habitat == "Water") |>
     # filter to on conc endpoints
     dplyr::left_join(db_endpoint_codes, by = c("endpoint" = "code")) |>
-    dplyr::filter(concentration_flag) |>
+    dplyr::filter(.data$concentration_flag) |>
     # clean up asterick endpoints
-    dplyr::mutate(endpoint = stringr::str_replace(endpoint, "\\*", "")) |>
+    dplyr::mutate(endpoint = stringr::str_replace(.data$endpoint, "\\*", "")) |>
     # add species info
     dplyr::left_join(db_species, by = "species_number") |>
     # add life stage info
     dplyr::left_join(
       db_lifestage_codes, by = c("organism_lifestage" = "code")
-    ) |>
-    # missing (NA) lifestages should be coded as adult
-    # doesn't appear to be any missing but adding in just in case
-    dplyr::mutate(
-      simple_lifestage = dplyr::if_else(
-        is.na(lifestage_description), "adult", simple_lifestage
-      )
     ) |>
     # add chemical info
     dplyr::left_join(db_chemicals, by = c("test_cas" = "cas_number")) |>
@@ -118,33 +113,33 @@ wqb_compile_dataset <- function(database) {
   
   compiled_data <- selected_columns_data |>
     # remove missing concentrations
-    dplyr::filter(!(conc1_mean == "NR")) |>
+    dplyr::filter(!(.data$conc1_mean == "NR")) |>
     # remove concentration with < or > in them
-    dplyr::filter(!(stringr::str_detect(conc1_mean, "\\<|\\>"))) |>
+    dplyr::filter(!(stringr::str_detect(.data$conc1_mean, "\\<|\\>"))) |>
     # remove rows with no species genus
-    dplyr::filter(!(genus == "")) |>
+    dplyr::filter(!(.data$genus == "")) |>
     # remove rows with no duration value
-    dplyr::filter(!(obs_duration_mean == "")) |> ### double check still valid 
-    dplyr::filter(!(obs_duration_mean == "NR")) |> ### double check still valid 
+    dplyr::filter(!(.data$obs_duration_mean == "")) |> ### double check still valid 
+    dplyr::filter(!(.data$obs_duration_mean == "NR")) |> ### double check still valid 
     dplyr::mutate(
       # remove asterisk from end point
-      endpoint = stringr::str_replace(endpoint, "\\*", ""),
+      endpoint = stringr::str_replace(.data$endpoint, "\\*", ""),
       # remove asterisk from conc1_mean values and convert to numeric
-      conc1_mean = stringr::str_replace(conc1_mean, "\\*", ""),
-      conc1_mean = as.numeric(conc1_mean),
+      conc1_mean = stringr::str_replace(.data$conc1_mean, "\\*", ""),
+      conc1_mean = as.numeric(.data$conc1_mean),
       # missing (NA) lifestages should be coded as adult
       # doesn't appear to be any missing but adding in just in case
       simple_lifestage = dplyr::if_else(
-        is.na(lifestage_description), 
+        is.na(.data$lifestage_description), 
         "adult", 
-        simple_lifestage
+        .data$simple_lifestage
       ),
       # simple life stage should only match for amphibians and fish
       simple_lifestage = dplyr::case_when(
-        ecological_group == "Invertebrate" ~ NA_character_,
-        ecological_group == "Algae" ~ NA_character_,
-        ecological_group == "Plant" ~ NA_character_,
-        TRUE ~ simple_lifestage
+        .data$ecological_group == "Invertebrate" ~ NA_character_,
+        .data$ecological_group == "Algae" ~ NA_character_,
+        .data$ecological_group == "Plant" ~ NA_character_,
+        TRUE ~ .data$simple_lifestage
       )
     ) 
   
