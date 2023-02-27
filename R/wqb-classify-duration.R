dbc4409452510e477f46bbc10bedf8ae10e3dedf
Ayla Pearson <ayla@poissonconsulting.ca>
2023-02-27 11:36:20 -0800

    framing up classify duration function for the package

---
+++ R/wqb-classify-duration.R
@@ -0,0 +1,15 @@
+#' Classify Duration 
+#' 
+#' Each test will be classified as an acute or chronic.
+#'
+#' @param data A data frame of the compiled data set. 
+#' @return Invisible data frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' classified_data <- wqb_classify_duration(compiled_data)
+#' }
+wqb_classify_duration <- function(data) {
+  
+}
