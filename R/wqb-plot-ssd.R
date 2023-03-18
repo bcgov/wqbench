d81bcb70f0b2f987aaa8f832410b676a59364d05
Ayla Pearson <ayla@poissonconsulting.ca>
2023-03-17 17:19:31 -0700

    fixing ssd result plot title having an underscore

--- R/wqb-plot-ssd.R
+++ R/wqb-plot-ssd.R
@@ -12,12 +12,13 @@ wqb_plot_ssd <- function(data, fit) {
   
   pred <- ssdtools::predict(fit, ci = TRUE)
   gp <- ssdtools::ssd_plot(
-    data, pred, shape = "trophic_group", color = "trophic_group", 
-    label = "common_name", left = "conc1_mean_std_effect_aggr_mg.L",
+    data, pred, shape = "ecological_group", color = "trophic_group", 
+    label = "latin_name", left = "conc1_mean_std_effect_aggr_mg.L",
     xlab = "Concentration (mg/L)", ribbon = TRUE
   ) + 
     ggplot2::expand_limits(x = 3000) +
-    ssdtools::scale_colour_ssd()
+    ssdtools::scale_colour_ssd() +
+    ggplot2::labs(color = "Trophic Group", shape = "Ecological Group")
   
   gp
 }
