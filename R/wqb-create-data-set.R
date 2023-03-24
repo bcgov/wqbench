85e0724f50536f5d602771fa4ede29dce43f29d1
Ayla Pearson <ayla@poissonconsulting.ca>
2023-03-24 13:08:04 -0700

    adding function to create whole app data set at once

---
+++ R/wqb-create-data-set.R
@@ -0,0 +1,65 @@
+# Copyright 2023 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Create the Data Set for the App
+#' 
+#' This function downloads the data, creates and adds all the data to the
+#' database, compiles, standardizes, and classifies the data. The output
+#' of this function is feed into the app. 
+#'
+#' @param file_path A string of the file path location to save the downloaded
+#'   files. The default is your current working directory.
+#' @param version An integer to indicate which version you want to download. The
+#'   default is 1 which downloads the most recent version.
+#' @param folder_path Folder path to write to.  
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  wqb_create_data_set(file_path = "~/Ecotoxicology/ecotox", 
+#'  version = 1, 
+#'  folder_path = "~/Ecotoxicology/ecotox_db/")
+#' }
+wqb_create_data_set <- function(file_path = "~/Ecotoxicology/ecotox", 
+                                version = 1, 
+                                folder_path = "~/Ecotoxicology/ecotox_db/") {
+  
+  ### check if already exists
+  ### ask to stop or to overwrite
+  
+  
+  data_path <- wqb_download_epa_ecotox(
+    file_path = file_path, version = version
+  )
+  
+  database <- wqb_create_epa_ecotox(
+    folder_path = folder_path,
+    data_path = data_path
+  )
+  
+  wqb_add_bc_species(database = database) 
+  wqb_add_bc_wqg(database = database)
+  wqb_add_concentration_endpoints(database = database)
+  wqb_add_lifestage(database = database) 
+  wqb_add_media(database = database)
+  wqb_add_trophic_group(database = database) 
+  wqb_add_duration_conversions(database = database)
+  wqb_add_conc_conversions(database = database)
+  
+  data <- wqb_compile_dataset(database = database) 
+  data <- wqb_classify_duration(data)
+  data <- wqb_standardize_effect(data)
+  data
+}
