34923a69c16d4a61ee11c14e952f238d15faa290
Ayla Pearson <ayla@poissonconsulting.ca>
2023-03-28 08:47:10 -0700

    adding licence to stop of scripts

--- tests/testthat/test-wqb-download-epa-ecotox.R
+++ tests/testthat/test-wqb-download-epa-ecotox.R
@@ -11,7 +11,3 @@ test_that("error when version lower then 1 is passed", {
     regexp = "`version` must be between 1 and 4, not 0\\."
   )
 })
-
-### may need to use withr to creates temp directory to test this step
-### those tests will be slow as the db is large to download
-### it could fail pending connection and other issues
