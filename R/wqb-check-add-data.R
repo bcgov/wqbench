5759e0dc0bc89abbe00b9c07af454f84e6730f9e
Ayla Pearson <ayla@poissonconsulting.ca>
2023-11-17 18:03:50 -0800

    add licences to new files

--- R/wqb-check-add-data.R
+++ R/wqb-check-add-data.R
@@ -1,3 +1,17 @@
+# Copyright 2023 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #' Check the Uploaded Data
 #'
 #' Checks the uploaded data for the basic requirements to ensure the data
@@ -109,8 +123,8 @@ check_species_present <- function(data) {
 check_no_extra_cols <- function(data, template) {
   if (length(setdiff(names(data), colnames(template)[-1]))) {
     chk::abort_chk(
-      "The data can only contain the columns from the template, please remove", 
-      chk::cc(setdiff(names(data), colnames(template)[-1]), conj = " or ")
+      "Only the columns from the template can be contained in the uploaded file, please remove: ", 
+      chk::cc(setdiff(names(data), colnames(template)[-1]), conj = " and ")
     )
   }
 }
