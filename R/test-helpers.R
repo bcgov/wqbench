6685a8abeb471258f31d314158bc00590bdfd84f
Ayla Pearson <ayla@poissonconsulting.ca>
2024-01-26 12:39:36 -0800

    adding snap shot tests for bcanz

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -106,3 +106,16 @@ expect_snapshot_plot <- function(x, name) {
   path <- save_png(x)
   testthat::expect_snapshot_file(path, paste0(name, ".png"))
 }
+
+expect_snapshot_data <- function(x, name, digits = 6) {
+  fun <- function(x) signif(x, digits = digits)
+  lapply_fun <- function(x) I(lapply(x, fun))
+  x <- dplyr::mutate(x, dplyr::across(where(is.numeric), fun))
+  x <- dplyr::mutate(x, dplyr::across(where(is.list), lapply_fun))
+  path <- save_csv(x)
+  testthat::expect_snapshot_file(
+    path, 
+    paste0(name, ".csv"), 
+    compare = testthat::compare_file_text
+  )
+}
