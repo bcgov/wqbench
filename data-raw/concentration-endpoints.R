9ac2e0b6d521236849ec0f455e973e96371b95ee
Ayla Pearson <ayla@poissonconsulting.ca>
2024-01-26 08:29:15 -0800

    adding log values into allowed codes

--- data-raw/concentration-endpoints.R
+++ data-raw/concentration-endpoints.R
@@ -14,10 +14,13 @@ lc_star <- paste0(lc, "*")
 non_numeric <- c("LOEC", "LOEL", "MATC", "MCIG", "NOEL", "NOEC")
 non_numeric_star <- paste0(non_numeric, "*")
 
+log_codes <-  c("(log)EC50", "(log)LC50", "(log)IC50")
+
 codes <- c(
   ec, ec_star,
   ic, ic_star,
   lc, lc_star,
+  log_codes,
   non_numeric,
   non_numeric_star
 )
@@ -25,4 +28,3 @@ codes <- c(
 conc_endpoints <- data.frame(code = codes)
 
 readr::write_csv(conc_endpoints, "inst/extdata/concentration-endpoints.csv")
-#usethis::use_data(concentration-endpoints, overwrite = TRUE)
