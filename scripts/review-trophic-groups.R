ba1ec9219103ccdeaa86987909f5bc85ae1b8848
Ayla Pearson <ayla@poissonconsulting.ca>
2024-01-28 08:33:36 -0800

    update how trophic group is reviewed

--- scripts/review-trophic-groups.R
+++ scripts/review-trophic-groups.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 # need to pull in raw database data
+# update the file to the most recent version of the database stored locally on your computer
 database <- "~/Ecotoxicology/ecotox_db/ecotox_ascii_12_14_2023.sqlite"
 
 con <- DBI::dbConnect(
@@ -15,64 +16,70 @@ db_species <- DBI::dbReadTable(con, "species") |>
   ) |>
   tibble()
 
-db_trophic_groups <- 
+DBI::dbDisconnect(con)
+
+missing_species <- 
   db_species |>
-  select(class, tax_order) |>
+  filter(is.na(trophic_group) | is.na(ecological_group))
+
+missing_groups <- 
+  missing_species |>
+  select(phylum_division, class, tax_order, family) |>
+  distinct() |>
   mutate(
+    phylum_division = str_squish(phylum_division),
     class = str_squish(class),
     tax_order = str_squish(tax_order),
-    class = na_if(class, ""),
-    tax_order = na_if(tax_order, ""),
-    in_db = TRUE
+    family = str_squish(family),
+    add = NA_character_,
+    across(c(phylum_division, class, tax_order, family), ~ na_if(.x, ""))
   ) |>
-  filter(!(is.na(class) & is.na(tax_order))) |>
-  rename(order = tax_order) |>
-  distinct() 
-
-# need to pull in compiled list
-trophic_groups_file_path <- system.file(
-  "extdata/trophic-group.csv",
-  package = "wqbench"
+  filter(
+    !(is.na(phylum_division) & is.na(class) & is.na(tax_order) & is.na(family))
+  ) |>
+  arrange(phylum_division, class, tax_order, family)
+  
+# generate files for review
+write_csv(
+  missing_species,
+  file.path(
+    "~",
+    "Poisson",
+    "Data",
+    "wqbench",
+    format(Sys.Date(), "%Y"),
+    "review",
+    paste0(Sys.Date(), "-species-not-coded-in-db", ".csv")
+  ),
+  na = ""
 )
 
-trophic_groups <- readr::read_csv(
-  trophic_groups_file_path,
-  show_col_types = FALSE
-) |>
-  mutate(
-    in_reference_data = TRUE
+write_csv(
+  missing_groups,
+  file.path(
+    "~",
+    "Poisson",
+    "Data",
+    "wqbench",
+    format(Sys.Date(), "%Y"),
+    "review",
+    paste0(Sys.Date(), "-missing-trophic-group-review", ".csv")
+  ),
+  na = ""
+)
+
+file.copy(
+  from = system.file(
+    "extdata/trophic-group.csv",
+    package = "wqbench"
+  ),
+  to = file.path(
+    "~",
+    "Poisson",
+    "Data",
+    "wqbench",
+    format(Sys.Date(), "%Y"),
+    "review",
+    paste0(Sys.Date(), "-trophic-group", ".csv")
   )
-
-# compare lists to see variation 
-trophic_group_list <- 
-  db_trophic_groups |>
-  full_join(trophic_groups, by = c("class", "order")) |>
-  arrange(class, order) 
-
-write_csv(
-  trophic_group_list,
-  file.path(
-    "~",
-    "Poisson",
-    "Data",
-    "wqbench",
-    format(Sys.Date(), "%Y"),
-    "review",
-    "trophic_group_review.csv"
-  ),
-  na = ""
-)
-
-write_csv(
-  db_species,
-  file.path(
-    "~",
-    "Poisson",
-    "Data",
-    "wqbench",
-    format(Sys.Date(), "%Y"),
-    "review",
-    "full_list_database_species.csv"
-  ),
-  na = ""
 )
